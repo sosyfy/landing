d943aa72b84849b36f669d9976d1182a73b717b0
sospeter <sospeterkaranja02@gmail.com>
2023-03-04 11:18:57 +0300

    Fix: padding issue on selectboxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,63 +15,63 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex flex-col mx-auto items-center max-w-[640px]">
         <div className="bg-[#121A35]  px-5 py-8">
-          <div className=' text-center'>
+          <div className='text-center '>
             <h1 className="text-[48px] tracking-[-0.04em] leading-[57px] font-bold text-white">Cancel on your <br /><span className="text-[#21DA49]">✌friends✌</span></h1>
             <p className='mt-4 text-[20px] tracking-[-0.05em] font-semibold leading-[30px] text-white/80'>Making cancelling plans easier.</p>
             <p className='text-[20px] tracking-[-0.05em] font-semibold leading-[30px] text-white/80'>All the joy, none of the guilt.</p>
           </div>
           <div className="rounded-[10px] bg-[#1A2754]/60 px-4 shadow py-3 mt-16 relative z-50">
             <form>
-              <div className='form-group mb-5'>
+              <div className='mb-5 form-group'>
                 <label htmlFor="plan" className="block text-[14px] leading-[17px] tracking-[-0.02em] mt-2 font-normal text-white mb-2">
                   Plan to cancel
                 </label>
                 <select
                   id="plan"
                   name="plan"
-                  className="select w-full  py-4 pl-2 rounded-[5px] border-gray-300 text-[15px] leading-[18px] tracking-[-2%]  focus:outline-none"
+                  className="select w-full py-4 pl-2 rounded-[5px] border-gray-300 text-[15px] leading-[18px] tracking-[-2%]  focus:outline-none"
                   defaultValue="Birthday"
                 >
                   <option className='w-fit'>Birthday</option>
                 </select>
               </div>
 
-              <div className='form-group mb-6'>
+              <div className='mb-6 form-group'>
                 <label htmlFor="excuse" className="block text-[14px] leading-[17px] tracking-[-0.02em] mt-2 font-normal text-white mb-2">
                   Excuse
                 </label>
                 <select
                   id="excuse"
                   name="excuse"
-                  className="block select w-full  py-[10px] pl-2 rounded-[5px] border-gray-300 text-base  focus:outline-none"
+                  className="select w-full py-4 pl-2 rounded-[5px] border-gray-300 text-[15px] leading-[18px] tracking-[-2%]  focus:outline-none"
                   defaultValue=""
                 >
                   <option>I'm pregnant again</option>
                 </select>
               </div>
 
-              <div className='form-group mb-6'>
+              <div className='mb-6 form-group'>
                 <label htmlFor="tone" className="block text-[14px] leading-[17px] tracking-[-0.02em] mt-2 font-normal text-white mb-2">
                   Tone
                 </label>
                 <select
                   id="tone"
                   name="tone"
-                  className="block select w-full  py-[10px] pl-2 rounded-[5px] border-gray-300 text-base  focus:outline-none"
+                  className="select w-full py-4 pl-2 rounded-[5px] border-gray-300 text-[15px] leading-[18px] tracking-[-2%]  focus:outline-none"
                   defaultValue=""
                 >
                   <option>Angry</option>
                 </select>
               </div>
 
-              <div className='form-group mb-6' >
+              <div className='mb-6 form-group' >
                 <label htmlFor="blame" className="block text-[14px] leading-[17px] tracking-[-0.02em] mt-2 font-normal text-white mb-2">
                   Blame
                 </label>
                 <select
                   id="blame"
                   name="blame"
-                  className="block select w-full  py-[10px] pl-2 rounded-[5px] border-gray-300 text-base  focus:outline-none"
+                  className="select w-full py-4 pl-2 rounded-[5px] border-gray-300 text-[15px] leading-[18px] tracking-[-2%]  focus:outline-none"
                   defaultValue=""
                 >
                   <option>You never believed in me</option>
@@ -79,15 +79,15 @@ const Home: NextPage = () => {
               </div>
 
               <div className="buttons">
-                <button type="submit" className='w-full py-[6px] leading-[30px] rounded-[5px] mb-4 text-center font-medium text-white text-[15px] bg-[#40D260]/70'>Justify your negligence</button>
-                <button type='button' className='w-full py-[6px] leading-[30px] rounded-[5px] mb-4 text-center font-medium text-white text-[15px] bg-gradient-to-r from-[#AC62E6] to-[#9755CB]'>Random</button>
+                <button type="submit" className='w-full py-2 leading-[30px] rounded-[5px] mb-4 text-center font-medium text-white text-[15px] bg-[#40D260]/70'>Justify your negligence</button>
+                <button type='button' className='w-full py-2 leading-[30px] rounded-[5px] mb-4 text-center font-medium text-white text-[15px] bg-gradient-to-r from-[#AC62E6] to-[#9755CB]'>Random</button>
               </div>
             </form>
 
             <div className="mt-4">
               <h1 className="mb-[14px] text-[20px] leading-[30px] tracking-[-0.05em] font-semibold text-white">Result:</h1>
               <div className="rounded bg-[#D9D9D9]/20 relative px-3 py-6">
-                <div className="icon absolute top-3 h-7 w-7 flex items-center justify-center right-3 bg-white rounded-full">
+                <div className="absolute flex items-center justify-center bg-white rounded-full icon top-3 h-7 w-7 right-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-5">
                     <path d="M9.25 13.25a.75.75 0 001.5 0V4.636l2.955 3.129a.75.75 0 001.09-1.03l-4.25-4.5a.75.75 0 00-1.09 0l-4.25 4.5a.75.75 0 101.09 1.03L9.25 4.636v8.614z" />
                     <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
